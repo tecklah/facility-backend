d8a22f8c179fa96f07f21b3a3fc3d21e2caeb4e2
Cheng Soon Teck <teck.lah@gmail.com>
2026-01-02 21:59:18 +0800

    Update

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ except Exception as e:
     db_pool = None
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://facility-frontend.web.app"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:4000", "https://facility-frontend.web.app"]}})
 
 @app.route('/login', methods=['POST'])
 def login_user_account():
